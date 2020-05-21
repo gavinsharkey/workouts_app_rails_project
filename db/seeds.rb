fd2f6dced8f6316701ac80ff305a162805fde215
gavinsharkey <gavinsharkey2016@gmail.com>
2020-05-21 18:42:45 -0400

    add ul of workouts to workouts#indexpage add faker gem seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Workout.destroy_all
+Exercise.destroy_all
+
+require 'open-uri'
+page = Nokogiri::HTML(open('https://guardyourhealth.tumblr.com/post/157357020660/a-to-z-workout'))
+page.css('div.article-content ul li')[0..25].each do |exercise_data|
+  text = exercise_data.text
+  name = text.split(':')[0]
+  description = text.split(':')[1].strip
+  Exercise.create(name: name, description: description)
+end
+
+workouts = ['Chest Day', 'Leg Day', 'Arm', 'Arm Day', 'Hella Pump', 'SICK WORKOUT', 'Glute Destroyer']
+
+5.times do
+  u = User.create(name: Faker::Name.first_name, email: Faker::Internet.email, password: 'password')
+  3.times do
+    w = u.workouts.create(name: workouts.sample, description: Faker::Lorem.sentence)
+    5.times do
+      e = Exercise.order(Arel.sql('RANDOM()')).first
+      w.custom_exercises.create(exercise: e)
+    end
+  end
+end
