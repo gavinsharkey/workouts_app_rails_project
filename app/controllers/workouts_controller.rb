835d66bfb42303b6bb982bd8602b17e489bbeb00
gavinsharkey <gavinsharkey2016@gmail.com>
2020-05-21 15:49:17 -0400

    configure workouts#index action

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,6 +2,12 @@ class WorkoutsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    
+    if params[:user_id]
+      @user = User.find_by(id: params[:user_id])
+      redirect_to workouts_path if !@user
+      @workouts = @user.workouts
+    else
+      @workouts = Workout.all
+    end
   end
 end
