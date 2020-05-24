c2197517a14e0fd53bcad8cb3fc009c2cbc3545f
gavinsharkey <gavinsharkey2016@gmail.com>
2020-05-24 19:37:08 -0400

    move UserSavedWorkout helper methods to user_saved_workout helpers Update users#show page to use helpers

--- app/helpers/user_saved_workouts_helper.rb
+++ app/helpers/user_saved_workouts_helper.rb
@@ -2,4 +2,16 @@ module UserSavedWorkoutsHelper
   def workout_saved?(workout)
     !!current_user.saved_workouts.exists?(workout.id)
   end
+
+  def title(workout)
+    workout.custom_title.blank? ? workout.saved_workout.name : workout.custom_title
+  end
+
+  def user_name(workout)
+    workout.saved_workout.user.name
+  end
+
+  def saved_workout_description(workout)
+    workout.saved_workout.description
+  end
 end
