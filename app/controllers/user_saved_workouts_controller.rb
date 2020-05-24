37d7c82a06cf1417b51b53ed643a9175e3cf1b9a
gavinsharkey <gavinsharkey2016@gmail.com>
2020-05-24 19:57:29 -0400

    Fix association method call for user_saved_workout on users#show page Add set_workout before_action Add set_saved_workout before action add and configure edit action update new, create, and destroy to use before action

--- app/controllers/user_saved_workouts_controller.rb
+++ app/controllers/user_saved_workouts_controller.rb
@@ -1,6 +1,8 @@
 class UserSavedWorkoutsController < ApplicationController
+  before_action :set_workout, only: [:new, :create, :edit]
+  before_action :set_saved_workout, only: [:destroy]
+
   def new
-    @workout = Workout.find_by(id: params[:workout_id])
     if @workout
       @saved_workout = @workout.user_saved_workouts.build
     else
@@ -10,7 +12,6 @@ class UserSavedWorkoutsController < ApplicationController
   end
 
   def create
-    @workout = Workout.find_by(id: params[:workout_id])
     @saved_workout = current_user.user_saved_workouts.build(saved_workout: @workout, custom_title: saved_workout_params[:custom_title])
 
     if @saved_workout.save
@@ -20,12 +21,37 @@ class UserSavedWorkoutsController < ApplicationController
     end
   end
 
+  def edit
+    if @workout
+      @saved_workout = @workout.user_saved_workouts.find_by(params[:id])
+      if !@saved_workout
+        flash[:alert] = 'Workout Not Saved'
+        redirect_to user_path(current_user)
+      end
+    else
+      flash[:alert] = 'Workout Not Found'
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def update
+
+  end
+
   def destroy
-    UserSavedWorkout.find(params[:id]).destroy
+    @saved_workout.destroy
     redirect_to workout_path(params[:workout_id])
   end
 
   private
+
+  def set_workout
+    @workout = Workout.find_by(id: params[:workout_id])
+  end
+
+  def set_saved_workout
+    @saved_workout = UserSavedWorkout.find_by(id: params[:id])
+  end
 
   def saved_workout_params
     params.require(:user_saved_workout).permit(:custom_title)
