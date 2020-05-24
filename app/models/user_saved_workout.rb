6aee56b81ff558bc429e9c817a392922db072df2
gavinsharkey <gavinsharkey2016@gmail.com>
2020-05-24 12:20:25 -0400

    add helper methods to UserSavedWorkout model (will refactor)

--- app/models/user_saved_workout.rb
+++ app/models/user_saved_workout.rb
@@ -1,4 +1,16 @@
 class UserSavedWorkout < ApplicationRecord
   belongs_to :saved_workout, class_name: 'Workout'
   belongs_to :saved_user, class_name: 'User'
+
+  def title
+    self.custom_title.blank? ? self.saved_workout.name : self.custom_title
+  end
+
+  def user_name
+    self.saved_workout.user.name
+  end
+
+  def saved_workout_description
+    self.saved_workout.description
+  end
 end
