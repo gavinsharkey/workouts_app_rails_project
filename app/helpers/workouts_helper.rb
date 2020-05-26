e47ecaf844a03199643428eaf2ae8e26203ad861
gavinsharkey <gavinsharkey2016@gmail.com>
2020-05-26 09:22:56 -0400

    Add current_users_workout? helper method

--- app/helpers/workouts_helper.rb
+++ app/helpers/workouts_helper.rb
@@ -2,4 +2,8 @@ module WorkoutsHelper
   def workout_user_name(workout)
     workout.user.name
   end
+
+  def current_users_workout?(workout)
+    current_user.workouts.exists?(workout.id)
+  end
 end
