b6348349db7e036ca22b6a945fb6b316175636be
gavinsharkey <gavinsharkey2016@gmail.com>
2020-05-26 10:27:25 -0400

    add helper for checking if a workout is saved by the current_user

--- app/helpers/user_saved_workouts_helper.rb
+++ app/helpers/user_saved_workouts_helper.rb
@@ -3,6 +3,10 @@ module UserSavedWorkoutsHelper
     !!current_user.saved_workouts.exists?(workout.id)
   end
 
+  def current_users_saved_workout?(user_saved_workout)
+    current_user.user_saved_workouts.exists?(user_saved_workout.id)
+  end 
+
   def title(workout)
     workout.custom_title.blank? ? workout.saved_workout.name : workout.custom_title
   end
