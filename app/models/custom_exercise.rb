d0e2027f400f4dc1438d27122e39dc83ccf95470
gavinsharkey <gavinsharkey2016@gmail.com>
2020-05-26 11:48:36 -0400

    CustomExercise validates presence of sets

--- app/models/custom_exercise.rb
+++ app/models/custom_exercise.rb
@@ -3,6 +3,7 @@ class CustomExercise < ApplicationRecord
   belongs_to :exercise
 
   validates :rep_range, presence: true
+  validates :sets, presence: true
 
   def exercise_name
     self.exercise.name
