5df0b1fe96866d76f100af46c8eeeaaafd0f1d00
gavinsharkey <gavinsharkey2016@gmail.com>
2020-05-27 11:39:03 -0400

    Configure create action for comments, creates comment nested under workout

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  def create
+    @workout = Workout.find_by(id: params[:workout_id])
+    @comment = @workout.comments.build(user: current_user, body: comment_params[:body])
+    if @comment.save
+      redirect_to workout_path(@workout)
+    else
+      @custom_exercises = @workout.custom_exercises
+      render 'workouts/show'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
