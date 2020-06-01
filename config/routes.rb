b98393a582a952a650de78cb06893aac312385e4
gavinsharkey <gavinsharkey2016@gmail.com>
2020-06-01 08:53:12 -0400

    Root to welcome#home add user_root helper to workouts#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'application#home'
+  root 'welcome#home'
 
   devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'omniauth_callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/signup', to: 'devise/registrations#new'
   end
+
+  get '/workouts', to: 'workouts#index', as: :user_root
 
   resources :users, only: [:show] do
     resources :workouts, only: [:index, :new, :create]
