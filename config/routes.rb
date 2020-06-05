c3ee1d0937e19f5822b38182b613b05a48764737
gavinsharkey <gavinsharkey2016@gmail.com>
2020-06-05 09:51:04 -0400

    refactor devise scope routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_scope :user do
     get '/login', to: 'devise/sessions#new'
-  end
-  devise_scope :user do
     get '/signup', to: 'devise/registrations#new'
   end
 
